9a881ae97ccf18e86a4cec3749a970553bbfbf60
Jacob McDonald <jacobmcdonald@btinternet.com>
2019-04-17 16:16:54 +0100

    Added serial code for linux

--- ponggame.py
+++ ponggame.py
@@ -1,22 +1,37 @@
 from ctypes import *
 import os
+import platform
 import constants
 
+
+if platform.system() == "Linux":
+    from serial import Serial
+
+    serialPort = Serial("/dev/ttyAMA0", 115200)
+
+    if serialPort.isOpen() == False:
+        serialPort.open()
+
+
+# Windows stuff
 STD_OUTPUT_HANDLE = -11
- 
+
 class COORD(Structure):
     pass
- 
 COORD._fields_ = [("X", c_short), ("Y", c_short)]
+# End windows stuff
 
 # This is for windows console
 # On Pi replace with ANSI escape sequence, where "\033[y;xH" moves the cursor to row y col x.
 def print_at(r, c, s):
-    h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
-    windll.kernel32.SetConsoleCursorPosition(h, COORD(c, r))
+    if platform.system() == "Windows":
+        h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(c, r))
  
-    c = s.encode("windows-1252")
-    windll.kernel32.WriteConsoleA(h, c_char_p(c), len(c), None, None)
+        c = s.encode("windows-1252")
+        windll.kernel32.WriteConsoleA(h, c_char_p(c), len(c), None, None)
+    elif platform.system() == "Linux":
+        serialPort.write(u"\033[" + r + ";" + c + "H" + s);
 
 
 class Point:
