3b3ba28265c460fd1ffcc775ec2a281ece3e81f1
Jacob McDonald <jacobmcdonald@btinternet.com>
2019-04-18 11:46:38 +0100

    Finished debug display

--- debugdisplay.py
+++ debugdisplay.py
@@ -2,12 +2,16 @@ import serialprint
 
 def printHardwareDebugHeader():
     print("\f")
-    print_at(1, 1, "Diagnostic Information:",)
-    print_at(2, 1, "|A ADC | A1 | A2 | Bat Pos | Size |")
-    print_at(3, 1, "|-------------------------------------|\r\n")
-    print_at(5, 1, "|B ADC | B1 | B2 | Bat Pos | Size |")
-    print_at(6, 1, "|-------------------------------------|\r\n")
+    serialprint.print_at(1, 1, "Diagnostic Information:",)
+    serialprint.print_at(2, 1, "| A ADC | A1 | A2 | Bat Pos | Size |")
+    serialprint.print_at(3, 1, "|----------------------------------|")
 
-def printHardwareDisplay(aval, a1, a2, bval, b1, b2):
-    print_at(4, 1, "| " + str(aval).rjust(3, " ") + "% |  " + str(a1) + " |  " + str(a2) + " |  ")
-    print_at(7, 1, "| " + str(bval).rjust(3, " ") + "% |  " + str(b1) + " |  " + str(b2) + " |\r")
+    serialprint.print_at(5, 1, "|----------------------------------|")
+    serialprint.print_at(6, 1, "| B ADC | B1 | B2 | Bat Pos | Size |")
+    serialprint.print_at(7, 1, "|----------------------------------|")
+
+    serialprint.print_at(9, 1, "|----------------------------------|")
+
+def printHardwareDisplay(aval, a1, a2, apos, asize, bval, b1, b2, bpos, bsize):
+    serialprint.print_at(4, 1, "|  " + str(aval).rjust(3) + "% |  " + str(a1) + " |  " + str(a2) + " | " + str(apos).rjust(3) + "/24  | " + str(asize).rjust(3) + "  |")
+    serialprint.print_at(8, 1, "|  " + str(bval).rjust(3) + "% |  " + str(b1) + " |  " + str(b2) + " | " + str(bpos).rjust(3) + "/24  | " + str(bsize).rjust(3) + "  |")
