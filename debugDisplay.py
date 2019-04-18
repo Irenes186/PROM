19c49052c5a708498696a8be44c60969c00cb3d8
Jacob McDonald <jacobmcdonald@btinternet.com>
2019-04-18 10:24:49 +0100

    Moved print_at and setColor into serialprint.py

--- debugDisplay.py
+++ debugDisplay.py
@@ -1,8 +1,8 @@
 
 def printHardwareDebugHeader():
-    print("Diagnostic Information:\r\n")
+    print("\fDiagnostic Information:\r\n")
     print("|A ADC | A1 | A2 | B ADC | B1 | B2 |\r\n")
     print("|----------------------------------|\r\n")
 
 def printHardwareDisplay(aval, a1, a2, bval, b1, b2):
-    print("| " + str(aval).zfill(3) + "% |  " + str(a1) + " |  " + str(a2) + " |  " + str(bval).zfill(3) + "% |  " + str(b1) + " |  " + str(b2) + " |\r")
+    print("| " + str(aval).rjust(3, " ") + "% |  " + str(a1) + " |  " + str(a2) + " |  " + str(bval).rjust(3, " ") + "% |  " + str(b1) + " |  " + str(b2) + " |\r")
---
+++ serialprint.py
@@ -0,0 +1,37 @@
+from ctypes import *
+import os
+import platform
+
+if platform.system() == "Linux":
+    from serial import Serial
+
+    serialPort = Serial("/dev/ttyAMA0", 115200)
+
+    if serialPort.isOpen() == False:
+        serialPort.open()
+
+
+# Windows stuff
+STD_OUTPUT_HANDLE = -11
+
+class COORD(Structure):
+    pass
+COORD._fields_ = [("X", c_short), ("Y", c_short)]
+# End windows stuff
+
+# This is for windows console
+# On Pi replace with ANSI escape sequence, where "\033[y;xH" moves the cursor to row y col x.
+def print_at(r, c, s):
+    if platform.system() == "Windows":
+        h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(c, r))
+ 
+        c = s.encode("windows-1252")
+        windll.kernel32.WriteConsoleA(h, c_char_p(c), len(c), None, None)
+    elif platform.system() == "Linux":
+        serialPort.write(u"\033[" + str(r+1) + ";" + str(c) + "H" + str(s));
+
+# Colour only works on linux
+def setColor(code):
+    if platform.system() == "Linux":
+        serialPort.write(u"\033[" + str(code) + "m")
