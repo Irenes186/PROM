84e1e246e8542c8dc6ef43c6d56ab328d717dbc1
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-04-23 10:34:35 +0100

    Update LEDDisplay.py

--- LEDDisplay.py
+++ LEDDisplay.py
@@ -1,1 +1,27 @@
+#switching LEDS on and off
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#to be changed with the actual value
+pin_list = [11,12, 13, 14, 15, 16, 17, 18]
+#the following pins will be used as outputs
+GPIO.setup(pin_list[0], GPIO.OUT)
+GPIO.setup(pin_list[1], GPIO.OUT)
+GPIO.setup(pin_list[2], GPIO.OUT)
+GPIO.setup(pin_list[3], GPIO.OUT)
+GPIO.setup(pin_list[4], GPIO.OUT)
+GPIO.setup(pin_list[5], GPIO.OUT)
+GPIO.setup(pin_list[6], GPIO.OUT)
+GPIO.setup(pin_list[7], GPIO.OUT)
+
+
+#turns on a LED every 3 seconds/ 8 LEDs time
+
+for i in range(0, len(pin_list)):
+    GPIO.output(pin_list[i], GPIO.HIGH)
+    time.sleep(0.375)
+
+    GPIO.output(pin_list[i], GPIO.LOW)
