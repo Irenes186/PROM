0ff35871096685ef6c250e86cdb273ec5c623dff
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-04-24 10:20:21 +0100

    Update LEDDisplay.py

--- LEDDisplay.py
+++ LEDDisplay.py
@@ -1,6 +1,7 @@
 #switching LEDS on and off
 import RPi.GPIO as GPIO
 import time
+import ponggame
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -15,8 +16,27 @@ for i in range(0, len(pin_list)):
 
 #turns on a LED every 3 seconds/ 8 LEDs time
 
-for i in range(0, len(pin_list)):
+"""for i in range(0, len(pin_list)):
     GPIO.output(pin_list[i], True)
     time.sleep(1)
 
     GPIO.output(pin_list[i], False)
+"""
+
+while  GameState == constants.STATE_IN_PLAY:
+    if constants.COLUMNS <= 10 :
+        GPIO.output(pin_list[0], True)
+    else if constants.COLUMNS > 10 and constants.COLUMNS <= 20:
+        GPIO.output(pin_list[1], True)
+    else if constants.COLUMNS > 20 and constants.COLUMNS <= 30:
+        GPIO.output(pin_list[2], True)
+    else if constants.COLUMNS > 30 and constants.COLUMNS <= 40:
+        GPIO.output(pin_list[3], True)
+    else if constants.COLUMNS > 40 and constants.COLUMNS <= 50:
+        GPIO.output(pin_list[4], True)
+    else if constants.COLUMNS > 50 and constants.COLUMNS <= 60:
+        GPIO.output(pin_list[5], True)
+    else if constants.COLUMNS > 60 and constants.COLUMNS <= 70:
+        GPIO.output(pin_list[6], True)
+    else:
+        GPIO.output(pin_list[7], True)
