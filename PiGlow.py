c63c1af04185be4ab42b37908fd6d6f9984bb826
Ryth-cs <49680490+Ryth-cs@users.noreply.github.com>
2019-04-24 15:05:16 +0100

    Update PiGlow.py

--- PiGlow.py
+++ PiGlow.py
@@ -1,7 +1,24 @@
+import time
+from PyGlow import PyGlow
 
-
+pyglow = PyGlow()
+	
 def blueWin():
-    pass
+	for i in range(0, 5):
+		pyglow.color("white", 50)
+		pyglow.color("blue", 200)
+		pyglow.color("green", 80)
+		pyglow.color("yellow", 50)
+		time.sleep(0.15)
+		pyglow.all(0)
+		time.sleep(0.15)
 
-def redWin();
-    pass
+def redWin():
+	for i in range(0, 5):
+			pyglow.color("white", 50)
+			pyglow.color("orange", 100)
+			pyglow.color("yellow", 50)
+			pyglow.color("red", 200)
+			time.sleep(0.15)
+			pyglow.all(0)
+			time.sleep(0.15)
