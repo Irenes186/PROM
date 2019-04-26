5304a0895dcc4909aa70ab626c6202c10a0484f7
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-04-26 13:59:32 +0100

    Update LEDboard.py

--- LEDboard.py
+++ LEDboard.py
@@ -4,9 +4,25 @@ I2C_ADDR = 0x20
 LED_ON = 0x00
 LED_OFF = 0xFF
 bus = smbus.SMBus(1)
+     
+def updateboard(GameState, position):
+    for i in range(0, len(pin_list)):
+        GPIO.output(pin_list[i], False)
 
-while True:
-     bus.write_byte( I2C_ADDR, LED_ON )
-     time.sleep(1)
-     bus.write_byte( I2C_ADDR, LED_OFF )
-     time.sleep(1)
+	#print("LEDDisp")
+    if position <= 10 :
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 10 and position <= 20:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 20 and position <= 30:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 30 and position <= 40:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 40 and position <= 50:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 50 and position <= 60:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    elif position > 60 and position <= 70:
+        bus.write_byte( I2C_ADDR, LED_ON )
+    else:
+        bus.write_byte( I2C_ADDR, LED_ON )
