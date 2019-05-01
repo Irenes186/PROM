9bfe6c71a012e33e0a22ffe42c18d83db85b5dc6
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-05-01 10:50:43 +0100

    Update LEDboard.py

--- LEDboard.py
+++ LEDboard.py
@@ -4,43 +4,33 @@ import smbus
 
 LED_ON = 0x00
 LED_OFF = 0xFF
+I2C = 0x38
 bus = smbus.SMBus(1)
   
 bus.write_byte(0x38, 0xF7)
 time.sleep(1)
 
-
+pins = [0xF1,0xF2, 0xF4, 0xF8,0x1F, 0x2F, 0x4F, 0x8F]
   
+
 def updateBoard(position):
-    for i in range(0, len(I2C_ADDR)):
-        bus.write_byte(I2C_ADDR[i], LED_OFF)
-
-	#print("LEDDisp")
+	#test
+    for i in range(0, len(pins)):
+        bus.write_byte(0x38, pins[i])
+	#game functionality
     if position <= 10 :
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[0])
     elif position > 10 and position <= 20:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[1])
     elif position > 20 and position <= 30:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[2])
     elif position > 30 and position <= 40:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[3])
     elif position > 40 and position <= 50:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[4])
     elif position > 50 and position <= 60:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[5])
     elif position > 60 and position <= 70:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
+        bus.write_byte(0x38, pins[6])
     else:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
-
-#updateBoard(11)
-    elif position > 40 and position <= 50:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
-    elif position > 50 and position <= 60:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
-    elif position > 60 and position <= 70:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
-    else:
-        bus.write_byte( I2C_ADDR[i], LED_ON )
-
-#updateBoard(11)
+        bus.write_byte(0x38, pins[7])
