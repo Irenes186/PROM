0870e31b79368274f4332809c62d8175f4889127
Ryth-cs <49680490+Ryth-cs@users.noreply.github.com>
2019-05-02 15:22:14 +0100

    Written ConvertToPosition

--- inputs.py
+++ inputs.py
@@ -8,9 +8,15 @@ I2CADDR = 0x21
 CMD_CODE = 0x10	# identify this from the data sheet
 bus = smbus.SMBus(1)
 
-def convertToPosition(number):
-	val = (number-950)/159
+def init():
+	print("Init")
+    #debugdisplay.printHardwareDebugHeader()
+
+def convertToPosition(ADC_Value, bat_size):
+	ADC_Step = (constants.ADC_highend - constants.ADC_lowend)/(constants.ROWS - (bat_size - 1))
+	val = (ADC_Value - constants.ADC_lowend) / ADC_Step
 	val = max(0, val)
+	val = min((constants.ROWS - (bat_size)), val)
 	return val
 
 def update(bat1, bat2, GameState):
@@ -21,7 +27,7 @@ def update(bat1, bat2, GameState):
 	decimalVal = str(reversedBits)[4:]
 	finalVal = int(decimalVal, 2)
 	#print("Decimal value = " + str(finalVal))
-	bat1.position.y = convertToPosition(finalVal)
+	bat1.position.y = convertToPosition(finalVal, bat1.length)
 	#print(bat1.position.y)
 	#time.sleep(0.2)
-	debugdisplay.printHardwareDisplay(1.5, 0, 1, bat1.position.y, bat1.length, 3, 0, 0, bat2.position.y, bat2.length)
+	#debugdisplay.printHardwareDisplay(finalVal, 0, 1, bat1.position.y, bat1.length, 3, 0, 0, bat2.position.y, bat2.length)
