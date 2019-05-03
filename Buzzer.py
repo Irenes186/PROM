23d1acab0b209079ee6c699381bdcf5d46f5fb7a
Ryth-cs <49680490+Ryth-cs@users.noreply.github.com>
2019-05-03 14:36:05 +0100

    Update Buzzer.py

--- Buzzer.py
+++ Buzzer.py
@@ -1,21 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 from threading import Timer
-
 import constants
 
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(constants.PINS["Buzzer"], OUTPUT)
+GPIO.setup(constants.PINS["Buzzer"], GPIO.OUT)
 
-buzzerPin = GPIO.PWM(constants.PINS["Buzzer"], 10)
-  
+buzzerPin = GPIO.PWM(constants.PINS["Buzzer"], 500)
+
 def playSequence(sequence):
+    print(sequence)
     buzzerPin.stop()
     if len(sequence) > 0:
-        buzzerPin.changeDutyCycle(constants.NOTES[sequence.pop(0)])
+        note = sequence.pop(0)
+        buzzerPin.ChangeFrequency(note[0])
         buzzerPin.start(50)
-        t = Timer(duration, playSequence, args=sequence)
+        t = Timer(note[1], playSequence, args=[sequence])
         t.start()
+
+playSequence([[440, 5]])
