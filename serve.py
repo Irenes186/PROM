f1e1a191888a9633efee9fe76b3e31e2ae7bb42e
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-05-07 11:27:30 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,1 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+import constants
+import math
 
+pins = [0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F, 0x7F]
+bus = smbus.SMBus(1)
+
+
+def serve_from_bat(position):
+    bus.write_byte(0x38, pins[int(lvl)])
