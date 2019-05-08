25d89ef413529eb1f0189a772e139d60b8d24738
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-05-08 10:43:56 +0100

    Update Buzzer.py

--- Buzzer.py
+++ Buzzer.py
@@ -2,7 +2,23 @@ import RPi.GPIO as GPIO
 import time
 from threading import Timer
 import constants
+E3 = 82.41
+E3 = 164.81
+C3 = 130.81
+G3 = 196
+G = 49
+A2 = 110
+B2 = 123.47
+Bb2 = 116.54
+A3 = 220.00
+F3 = 174.61
+D3 = 146.83
+F3H = 185
 
+
+X =  [E3, E3, E3, C3, E3, G3, G, C3, G, E3, A2, 
+      B2, Bb2, A2, G, E3, G3, A3, F3, G3, C3, 
+      D3, B, G3, F3H, F3, D3, E3]
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -21,4 +37,5 @@ def playSequence(sequence):
         t = Timer(note[1], playSequence, args=[sequence])
         t.start()
 
-playSequence([[440, 5]])
+for i in range(0, len(X)):
+      playSequence([[X[i], 5]])
