1638b11ef62c313b5480582749fdf1731515a69e
Irenes186 <49714089+Irenes186@users.noreply.github.com>
2019-05-09 14:04:15 +0100

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -7,7 +7,12 @@ import time
 pins = [0xFE, 0xFD, 0xFB, 0xF7]
 I2CADDR = 0x21
 CMD_CODE = 0x10	# identify this from the data sheet
+I2C_ADDR_1 = 0x20
+PORT_ON = 0xFF
 bus = smbus.SMBus(1)
+
+pin_list = [0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F, 0x7F]
+pin_values = [False]
 
 def init():
 	print("Init")
@@ -32,4 +37,18 @@ def update(bat1, bat2, GameState):
 	#print(bat1.position.y)
 	#time.sleep(0.2)
 	debugdisplay.printHardwareDisplay(finalVal, 0, 1, bat1.position.y, bat1.length, 3, 0, 0, bat2.position.y, bat2.length)
+	
+	
+bus.write_byte(I2C_ADDR_1, PORT_ON )
+
+A= bus.read_byte(I2C_ADDR_1, pin_list[])
+B= bus.read_byte(I2C_ADDR_1, pin_list[])
+C= bus.read_byte( I2C_ADDR_1, pin_list[]) 
+D= bus.read_byte( I2C_ADDR_1, pin_list[])
+E= bus.read_byte( I2C_ADDR_1, pin_list[])
+F= bus.read_byte( I2C_ADDR_1, pin_list[])
+G= bus.read_byte( I2C_ADDR_1, pin_list[])
+H= bus.read_byte( I2C_ADDR_1, pin_list[])
+
+result = A*1 + B*2 + C*4 + D*8 + E*16 + F*32 + G*64 + H*128
 	
